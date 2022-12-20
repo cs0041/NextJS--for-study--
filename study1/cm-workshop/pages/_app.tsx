b244005d082ae01722e0ffeaeaa4d5ca0b539103
cs0041 <pamzalove20@hotmail.com>
2022-12-20 15:59:27 +0700

    theme

--- study1/cm-workshop/pages/_app.tsx
+++ study1/cm-workshop/pages/_app.tsx
@@ -4,18 +4,50 @@ import { store } from '@/store/store'
 import { Provider } from 'react-redux'
 import { getSession } from '@/store/slices/userSlice'
 import * as React from 'react'
-
+import { createTheme, ThemeProvider } from '@mui/material'
+import { blue ,red} from '@mui/material/colors'
 
 export default function App({ Component, pageProps }: AppProps) {
-     React.useEffect(() => 
-     {
-       store.dispatch(getSession())
-     }, [])
+  React.useEffect(() => 
+  {
+    store.dispatch(getSession())
+  }, [])
 
-  
+  const drawerWidth = 240
+
+   const theme = createTheme({
+     components: {
+       MuiDrawer: {
+         styleOverrides: {
+           paper: {
+             backgroundImage: 'url("/static/img/background_menu.jpg")',
+             backgroundRepeat: 'no-repeat',
+             backgroundPosition: 'bottom',
+             width: drawerWidth,
+           },
+         },
+       },
+     },
+     typography: {
+       fontFamily: 'Roboto',
+       fontWeightLight: 300,
+       fontWeightRegular: 400,
+       fontWeightMedium: 500,
+       fontWeightBold: 700,
+     },
+     spacing: 8,
+     palette: {
+       primary: process.env.NEXT_PUBLIC_IS_PRODUCTION == '0' ? red : blue,
+       background: {
+         default: '#FFF',
+       },
+     },
+   })
    return (
      <Provider store={store}>
-       <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
      </Provider>
    )
 }
